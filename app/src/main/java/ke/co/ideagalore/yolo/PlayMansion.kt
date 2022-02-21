b2fad665e9e20f17cb7153e70cd20b5428fc6829
w.kariukijames <kwjames9326@gmail.com>
2022-02-21 15:06:05 +0300

    Take One Yolo Test Play Mansion Added Successfully. New base established Sir.

---
+++ app/src/main/java/ke/co/ideagalore/yolo/PlayMansion.kt
@@ -0,0 +1,11 @@
+package ke.co.ideagalore.yolo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayMansion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_mansion)
+    }
+}
